41babefc5e2107ae70d8630072d2499e3766e7d9
rahuldav9771 <rahuldav9771@gmail.com>
2026-04-25 13:31:12 +0530

    filchaned

--- Environment/Dev/backend.tf
+++ Environment/Dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "backend_rg"
-    storage_account_name = "backendstorage9771"
-    container_name       = "backend-container"
+    storage_account_name = "backend9771"
+    container_name       = "backend"
     key                  = "terraform.tfstate"
     
   }
