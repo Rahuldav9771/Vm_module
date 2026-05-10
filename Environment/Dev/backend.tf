537c93645accd331009b4a4fbe58464db7d41ae5
Rahuldav9771 <51904476+Rahuldav9771@users.noreply.github.com>
2026-05-10 13:08:10 +0530

    Update storage account name in backend configuration
    Update storage account name in backend configuration

--- Environment/Dev/backend.tf
+++ Environment/Dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "backend_rg"
-    storage_account_name = "backend9771"
+    storage_account_name = "backend97716"
     container_name       = "backend"
     key                  = "terraform.tfstate"
     
